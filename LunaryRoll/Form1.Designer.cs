1884a2806ddea164c5c39cbfbf343c99f987708e
matheus.dsousa1 <matheus.dsousa1@AME0556343W10-1>
2025-01-27 11:49:40 -0300

    Adicionar arquivos de projeto.

---
+++ LunaryRoll/Form1.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace LunaryRoll
+{
+    partial class buttonMostrarSenha
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.inputUsuario = new System.Windows.Forms.TextBox();
+            this.inputSenha = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+            this.buttonEntrar = new System.Windows.Forms.Button();
+            this.buttonLimpar = new System.Windows.Forms.Button();
+            this.mensagemLogin = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.imagemOlho = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imagemOlho)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Verdana", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(507, 104);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(134, 45);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Login";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(421, 175);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 2;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Gray;
+            this.groupBox1.Controls.Add(this.imagemOlho);
+            this.groupBox1.Controls.Add(this.mensagemLogin);
+            this.groupBox1.Controls.Add(this.buttonLimpar);
+            this.groupBox1.Controls.Add(this.buttonEntrar);
+            this.groupBox1.Controls.Add(this.linkLabel2);
+            this.groupBox1.Controls.Add(this.linkLabel1);
+            this.groupBox1.Controls.Add(this.inputUsuario);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.inputSenha);
+            this.groupBox1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.groupBox1.ForeColor = System.Drawing.Color.Transparent;
+            this.groupBox1.Location = new System.Drawing.Point(336, 162);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(476, 281);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(29, 29);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Usuário";
+            // 
+            // inputUsuario
+            // 
+            this.inputUsuario.Location = new System.Drawing.Point(32, 45);
+            this.inputUsuario.Name = "inputUsuario";
+            this.inputUsuario.Size = new System.Drawing.Size(413, 20);
+            this.inputUsuario.TabIndex = 1;
+            // 
+            // inputSenha
+            // 
+            this.inputSenha.Location = new System.Drawing.Point(32, 89);
+            this.inputSenha.Name = "inputSenha";
+            this.inputSenha.PasswordChar = '*';
+            this.inputSenha.Size = new System.Drawing.Size(413, 20);
+            this.inputSenha.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(29, 73);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Senha";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.LinkColor = System.Drawing.Color.LightSkyBlue;
+            this.linkLabel1.Location = new System.Drawing.Point(32, 115);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(102, 13);
+            this.linkLabel1.TabIndex = 2;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Esqueceu a senha?";
+            // 
+            // linkLabel2
+            // 
+            this.linkLabel2.AutoSize = true;
+            this.linkLabel2.LinkColor = System.Drawing.Color.LightSkyBlue;
+            this.linkLabel2.Location = new System.Drawing.Point(32, 151);
+            this.linkLabel2.Name = "linkLabel2";
+            this.linkLabel2.Size = new System.Drawing.Size(63, 13);
+            this.linkLabel2.TabIndex = 2;
+            this.linkLabel2.TabStop = true;
+            this.linkLabel2.Text = "Cadastra-se";
+            this.linkLabel2.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel2_LinkClicked);
+            // 
+            // buttonEntrar
+            // 
+            this.buttonEntrar.ForeColor = System.Drawing.Color.Black;
+            this.buttonEntrar.Location = new System.Drawing.Point(371, 245);
+            this.buttonEntrar.Name = "buttonEntrar";
+            this.buttonEntrar.Size = new System.Drawing.Size(95, 26);
+            this.buttonEntrar.TabIndex = 3;
+            this.buttonEntrar.Text = "Entrar";
+            this.buttonEntrar.UseVisualStyleBackColor = true;
+            this.buttonEntrar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // buttonLimpar
+            // 
+            this.buttonLimpar.ForeColor = System.Drawing.Color.Black;
+            this.buttonLimpar.Location = new System.Drawing.Point(270, 245);
+            this.buttonLimpar.Name = "buttonLimpar";
+            this.buttonLimpar.Size = new System.Drawing.Size(95, 26);
+            this.buttonLimpar.TabIndex = 3;
+            this.buttonLimpar.Text = "Limpar Campos";
+            this.buttonLimpar.UseVisualStyleBackColor = true;
+            this.buttonLimpar.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // mensagemLogin
+            // 
+            this.mensagemLogin.AutoSize = true;
+            this.mensagemLogin.Location = new System.Drawing.Point(282, 112);
+            this.mensagemLogin.Name = "mensagemLogin";
+            this.mensagemLogin.Size = new System.Drawing.Size(0, 13);
+            this.mensagemLogin.TabIndex = 5;
+            this.mensagemLogin.Click += new System.EventHandler(this.mensagemLogin_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::LunaryRoll.Properties.Resources.momoi;
+            this.pictureBox3.Location = new System.Drawing.Point(322, 16);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(109, 110);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 4;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::LunaryRoll.Properties.Resources.momoi;
+            this.pictureBox2.Location = new System.Drawing.Point(718, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(107, 110);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // imagemOlho
+            // 
+            this.imagemOlho.BackColor = System.Drawing.SystemColors.Window;
+            this.imagemOlho.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.imagemOlho.Image = global::LunaryRoll.Properties.Resources.fechar_o_olho;
+            this.imagemOlho.Location = new System.Drawing.Point(420, 89);
+            this.imagemOlho.Name = "imagemOlho";
+            this.imagemOlho.Size = new System.Drawing.Size(25, 20);
+            this.imagemOlho.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.imagemOlho.TabIndex = 4;
+            this.imagemOlho.TabStop = false;
+            this.imagemOlho.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::LunaryRoll.Properties.Resources.Logo_LunaryRoll;
+            this.pictureBox1.Location = new System.Drawing.Point(401, 44);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(345, 57);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // buttonMostrarSenha
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.ClientSize = new System.Drawing.Size(1160, 511);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox3);
+            this.ForeColor = System.Drawing.Color.Transparent;
+            this.Name = "buttonMostrarSenha";
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imagemOlho)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox inputSenha;
+        private System.Windows.Forms.TextBox inputUsuario;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.LinkLabel linkLabel2;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Button buttonLimpar;
+        private System.Windows.Forms.Button buttonEntrar;
+        private System.Windows.Forms.PictureBox imagemOlho;
+        private System.Windows.Forms.Label mensagemLogin;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+    }
+}
+
---
+++ LunaryRoll/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LunaryRoll
+{
+    public partial class buttonMostrarSenha : Form
+    {
+        bool VerSenhaTxt = false;
+        private string usuarioCorreto = "admLunar";
+        private string senhaCorreta = "senhaFoda123";
+
+        public buttonMostrarSenha()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void linkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string usuario = inputUsuario.Text;
+            string senha = inputSenha.Text;
+
+            if (usuario == usuarioCorreto && senha == senhaCorreta)
+            {
+                mensagemLogin.Text = "Login efetuado com sucesso";
+                mensagemLogin.ForeColor = Color.GreenYellow;
+            } else
+            {
+                mensagemLogin.Text = "Usuário ou senha estão inválidas";
+                mensagemLogin.ForeColor= Color.OrangeRed;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            inputUsuario.Text = "";
+            inputSenha.Text = "";
+            inputUsuario.Focus();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            if (VerSenhaTxt == false)
+            {
+                inputSenha.PasswordChar = '\0';
+                VerSenhaTxt = true;
+                imagemOlho.Image = LunaryRoll.Properties.Resources.olho;
+ 
+            }
+            else
+            {
+                inputSenha.PasswordChar = '*';
+                VerSenhaTxt = false;
+                imagemOlho.Image = LunaryRoll.Properties.Resources.fechar_o_olho;
+            }
+        }
+
+        private void mensagemLogin_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
